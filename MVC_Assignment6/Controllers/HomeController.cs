6b5ba48f64210e444afc2dceed9aaefa3f77c055
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2022-05-02 15:50:13 +0530

    Added MVC_Assignment 6

---
+++ MVC_Assignment6/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Day6MvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day6MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+    }
+}
